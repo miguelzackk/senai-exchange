8f08e40507dd959832e52665625a2bed58dd788b
miguelzackk <mzsilvaw@gmail.com>
2025-11-05 21:23:30 -0300

    fix: bugs resolved

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -91,3 +91,23 @@ toggle.addEventListener("click", () => {
 
 
 });
+
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const perfilBtn = document.getElementById("perfil-btn");
+const closeBtn = document.getElementById("close-menu");
+
+perfilBtn.addEventListener("click", () => {
+  sidebar.classList.add("open");
+  overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  overlay.classList.remove("active");
+});
